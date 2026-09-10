015906d50430886dc47faea3de6897faed24b153
buraga-kyo <sprtj@protonmail.com>
2026-09-10 10:48:54 -0300

    teste(canal): prova prazo único da carga
    A prova envia o cabeçalho e uma carga parcial antes de um prazo curto e confirma a restituição da carga.
    
    Refs: #45
    
    Feito por buraga-kyo <sprtj@protonmail.com>

--- testes/provar_canal_de_governo.c
+++ testes/provar_canal_de_governo.c
@@ -1,8 +1,12 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
 #include "../src/canal_de_governo.h"
 
 #include <errno.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <time.h>
 #include <unistd.h>
 
 /*
@@ -72,6 +76,33 @@ termo:
     return resultado;
 }
 
+static int provar_prazo_absoluto_da_carga(void)
+{
+    unsigned char cabecalho[TAMANHO_DO_CABECALHO_DE_GOVERNO];
+    struct mensagem_de_governo mensagem = {0};
+    struct timespec agora;
+    int tomadas[2] = {-1, -1};
+    int resultado = 0;
+    if (socketpair(AF_UNIX, SOCK_STREAM, 0, tomadas) != 0) goto termo;
+    if (escrever_cabecalho_de_governo(
+            cabecalho, sizeof(cabecalho), OPERACAO_DE_GOVERNO_CREAR, 4) < 0)
+        goto termo;
+    if (send(tomadas[0], cabecalho, sizeof(cabecalho), 0) !=
+            (ssize_t)sizeof(cabecalho) || send(tomadas[0], "x", 1, 0) != 1)
+        goto termo;
+    if (clock_gettime(CLOCK_MONOTONIC, &agora) != 0) goto termo;
+    if (receber_mensagem_de_governo_com_prazo(
+            tomadas[1], &mensagem,
+            (uint64_t)agora.tv_sec * 1000000000ULL + agora.tv_nsec +
+                1000000ULL) != -ETIMEDOUT || mensagem.carga != 0) goto termo;
+    resultado = 1;
+termo:
+    destruir_mensagem_de_governo(&mensagem);
+    if (tomadas[0] >= 0) close(tomadas[0]);
+    if (tomadas[1] >= 0) close(tomadas[1]);
+    return resultado;
+}
+
 /*
  * Proposito: reunir as demonstrações portáteis do canal de governo.
  * Pre-condições: nenhuma tomada nomeada ou privilégio é necessário.
@@ -81,5 +112,6 @@ termo:
 int main(void)
 {
     return provar_transporte_do_canal_de_governo() &&
-           provar_carga_truncada_no_canal() ? 0 : 1;
+           provar_carga_truncada_no_canal() &&
+           provar_prazo_absoluto_da_carga() ? 0 : 1;
 }
